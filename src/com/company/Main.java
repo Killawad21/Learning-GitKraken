6a1b55258f89c3974a7b455c4125fe1b36116f04
Killawad21 <02logan.pennock@gulllakecs.org>
2019-11-12 09:35:57 -0500

    Add helloWorld

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
 public class Main {
-
+        public static void helloWorld(){
+            System.out.println("Hello World");
+        }
     public static void main(String[] args) {
+        helloWorld();
 	// write your code here
     }
+
 }

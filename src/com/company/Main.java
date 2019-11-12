b07b12ca494fb3eaf6c71c7f0ee7666a708275e1
Killawad21 <02logan.pennock@gulllakecs.org>
2019-11-12 09:53:35 -0500

    add weather

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
 public class Main {
-        public static void helloWorld(){
-            System.out.println("Hello World");
+        public static void weather(){
+            System.out.println("It is snowing");
         }
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
     public static void main(String[] args) {
         helloWorld();
+        weather();
 	// write your code here
     }
 

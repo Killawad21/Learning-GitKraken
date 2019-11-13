afd3b867116b3a7cb3c79add2ed557300d6a7855
Killawad21 <48028251+Killawad21@users.noreply.github.com>
2019-11-13 09:29:54 -0500

    Add Sports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,13 @@ package com.company;
 public class Main {
         public static void helloWorld(){
             System.out.println("Hello World");
+	}
+	public static void sport(){
+            System.out.println("you play football");
         }
     public static void main(String[] args) {
         helloWorld();
+	SPORT();
 	// write your code here
     }
 
